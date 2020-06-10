f78bbe7c0c6566120ec41a68042b9c425ac20881
Karolina Jurczak <inka_c@tlen.pl>
2020-06-10 22:39:10 +0200

    Add Bookings Events and Order views

--- src/components/views/Event/Event.js
+++ src/components/views/Event/Event.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import styles from './Event.module.scss';
+import Paper from '@material-ui/core/Paper';
+
+const demoContent = [
+  {id: '1', status: 'booking', hour: '10:30', eventName: 'birthday'},
+  {id: '2', status: 'booking', hour: '10:30', eventName: ''},
+  {id: '3', status: 'events', hour: '10:30', eventName: 'pizza time'},
+  {id: '4', status: 'booking', hour: '10:30', eventName: ''},
+  {id: '5', status: 'events', hour: '10:30', eventName: 'happy hours'},
+  {id: '6', status: 'booking', hour: '10:30', eventName: ''},
+];
 
 const Event = (value) => {
   const id = value.match.params.id;
   
+  const content = () => {
+    const info = [];
+    demoContent.map(row => {
+      if (row.id === id){
+        info.push(
+          <div key={id}>
+            <p>Id: {row.id}</p>
+            <p>Hour: {row.hour}</p>
+            <p>Status: {row.status}</p>
+            <p>Event name: {row.eventName}</p>
+          </div>
+        );
+      }
+    });
+    return info;
+  };
+  
   return (
-    <div className={styles.component}>
-      <h2 className={styles.title}>
-        Event {id}
-      </h2>
-    </div>
+    <Paper className={styles.component}>
+      <h3 className={styles.title}>
+        Informations
+      </h3>
+      <div>
+        {content()}
+      </div>
+    </Paper>
   );
 };
 
--- src/components/views/Order/Order.js
+++ src/components/views/Order/Order.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import styles from './Order.module.scss';
+import Paper from '@material-ui/core/Paper';
+
+const demoContent = [
+  {id: '1', tableNo: 1, status: 'READY', menu: 'pizza drinks', drinks: 'drink', price: '30PLN'},
+  {id: '2', tableNo: 3, status: 'IN DELIVERY', menu: 'pizza drinks', drinks: 'drink', price: '30PLN'},
+  {id: '3', tableNo: 6, status: 'ORDERED', menu: 'pizza drinks pizza drinks pizza drinks', drinks: 'drink', price: '30PLN'},
+  {id: '4', tableNo: 2, status: 'ORDERED', menu: 'pizza drinks pizza drinks', drinks: 'drink', price: '30PLN'},
+  {id: '5', tableNo: 4, status: 'ORDERED', menu: 'pizza drinks', drinks: 'drink', price: '30PLN'},
+  {id: '6', tableNo: 5, status: 'ORDERED', menu: 'pizza drinks', drinks: 'drink', price: '30PLN'},
+];
+
 
 const Order = (value) => {
   const id = value.match.params.id;
   
+  const content = () => {
+    const info = [];
+    demoContent.map(row => {
+      if (row.id === id){
+        info.push(
+          <div key={id}>
+            <p>Id: {row.id}</p>
+            <p>Table no: {row.tableNo}</p>
+            <p>Status: {row.status}</p>
+            <p>Menu: {row.menu}</p>
+            <p>Drinks: {row.drinks}</p>
+            <p>Price: {row.price}</p>
+          </div>
+        );
+      }
+    });
+    return info;
+  };
+  
   return (
-    <div className={styles.component}>
-      <h2 className={styles.title}>
-        Order {id}
-      </h2>
-    </div>
+    <Paper className={styles.component}>
+      <h3 className={styles.title}>
+        Order
+      </h3>
+      <div>
+        {content()}
+      </div>
+    </Paper>
   );
 };
 
