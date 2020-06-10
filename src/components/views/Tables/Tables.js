0089382f9c17d9b4b8b9bb6fddf1231f7ecac41f
Karolina Jurczak <inka_c@tlen.pl>
2020-06-10 12:48:40 +0200

    Add pages Waiter and Tables

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,20 +1,96 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+
+const demoContent = [
+  {id: 1, status: 'booked', eventName: 'birthday'},
+  {id: 2, status: 'booked', eventName: ''},
+  {id: 3, status: 'event', eventName: 'pizza time'},
+  {id: 4, status: 'booked', eventName: ''},
+  {id: 5, status: 'event', eventName: 'happy hours'},
+  {id: 6, status: 'booked', eventName: ''},
+];
 
 const Tables = () => {
+
+  const hour = (time) => {
+    const item = [];
+    for(let i = time; i < 10; i++) 
+    {
+      item.push(<TableRow key={i}>
+        <TableCell>{i}</TableCell>
+        {demoContent.map(row => (
+          i === row.id ? (
+            <TableCell key={row.id}>
+              <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/${row.id}`}>
+                {`${row.eventName} ${row.status} `}
+              </Button>
+            </TableCell>
+          ) :
+            (<TableCell key={row.id} />)
+        ),)}
+      </TableRow>);
+    }
+    return(item);
+  };
+
   return (
-    <div className={styles.component}>
-      <h2 className={styles.title}>
-        Tables view
-      </h2>
-      <nav>
-        <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`} > Booking</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} >New Booking</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables/events/:id`} >Event</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables/events/new`} >New Event</Link>
-      </nav>
-    </div>
+    <Paper className={styles.component}>
+      <div>
+        <Grid container justify="space-around">
+          <form className={styles.container} noValidate>
+            <TextField
+              id="date"
+              label="Select date"
+              type="date"
+              defaultValue="2020-06-10"
+              className={styles.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              id="time"
+              label="Select time"
+              type="time"
+              defaultValue="10:30"
+              // min="10:00"
+              // max="22:00"
+              // step=1800
+              className={styles.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </form>
+        </Grid>
+      </div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            {demoContent.map(row => (
+              <TableCell key={row.id}>
+                Table no {row.id}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {hour(1)}
+        </TableBody>
+      </Table>
+    </Paper>
+
   );
 };
 
